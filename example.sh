eee9b4e12d95b333e54f08128e28400128f7b337
djadar <mukamaugusta5@gmail.com>
2024-07-16 10:19:44 +0200

    add aupe in example.sh

--- example.sh
+++ example.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-echo "In this setting, Basalt should succeed:"
+aco "Try Basalt"
 cargo run -- -T 200 -n 1000 basalt -G -H -f 10 -t 300 -v 50 -i 50 -k 10 -r 10 
 
-echo "With the same parameters, Brahms should fail:"
+echo "Try Brahms"
 cargo run -- -T 200 -n 1000 brahms -G samples -f 10 -t 300 -v 50 -u 50 -k 10 -r 10 
+
+echo "Try Aupe"
+cargo run -- -T 200 -n 1000 aupe -G samples -f 10 -t 300 -v 50 -u 50 -k 10 -r 10 -m 100
