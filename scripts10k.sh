5e1050e17d7fa519a267deec3436a1355a7c2f86
djadar <mukamaugusta5@gmail.com>
2024-08-03 14:34:20 +0200

    test aupe-global on 400 rounds

--- scripts10k.sh
+++ scripts10k.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-wait
-mkdir rho0
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 800 -v 160 -u 160 -k 0 -r 1 > rho0/text8 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 1000 -v 160 -u 160 -k 0 -r 1 > rho0/text10 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 1200 -v 160 -u 160 -k 0 -r 1 > rho0/text12 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 1400 -v 160 -u 160 -k 0 -r 1 > rho0/text14 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 1600 -v 160 -u 160 -k 0 -r 1 > rho0/text16 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 1800 -v 160 -u 160 -k 0 -r 1 > rho0/text18 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 2000 -v 160 -u 160 -k 0 -r 1 > rho0/text20 &
-wait
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 2200 -v 160 -u 160 -k 0 -r 1 > rho0/text22 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 2400 -v 160 -u 160 -k 0 -r 1 > rho0/text24 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 2600 -v 160 -u 160 -k 0 -r 1 > rho0/text26 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 2800 -v 160 -u 160 -k 0 -r 1 > rho0/text28 &
-cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 3000 -v 160 -u 160 -k 0 -r 1 > rho0/text30 &
-#cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 3600 -v 160 -u 160 -k 0 -r 1 > rho0/text36 &
-#cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 4000 -v 160 -u 160 -k 0 -r 1 > rho0/text40 &
-#cargo run -- -T 200 -n 10000 brahms -G samples -f 10 -t 5000 -v 160 -u 160 -k 0 -r 1 > rho0/text50 
+simu=N="${1:-0}"
+echo "received instruction for " $simu
+
+N=10000
+v=160
+force=10
+sm=100
+roundMax=200
+
+mkdir $strat
+if [ $strat -eq "global" ]; then
+    # f=22% rho=1
+    cargo run -- -T 400 -n 10000 aupe -L -G samples -f $force -t 2200 \
+        -v 160 -u 160 -k 1 -r 1 > $strat"/rho0text22"
+elif [ $strat -eq "merge" ]; then
+    # f=22% rho=1
+    cargo run -- -T 400 -n 10000 aupe -O -G samples -f $force -t 2200 \
+        -v 160 -u 160 -k 1 -r 1 > $strat"/rho0text22"
+fi
