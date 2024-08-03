42ccccdf6017d1ee4164fec4037297b4a101b814
djadar <mukamaugusta5@gmail.com>
2024-08-03 11:51:40 +0200

    add trusted nodes

--- example.sh
+++ example.sh
@@ -11,3 +11,7 @@ cargo run -- -T 200 -n 1000 aupe -G samples -f 10 -t 200 -v 100 -u 100 -k 1 -r 1
 cargo run -- -T 10 -n 10 aupe -G samples -f 10 -t 3 -v 5 -u 5 -k 1 -r 1 -m 10 -n 10
 cargo run -- -T 200 -n 10000 aupe -O -G samples -f 10 -t 2000 -v 160 -u 160 -k 1 -r 1 -m 100 -n 10000 # aupe-merge
 cargo run -- -T 200 -n 10000 aupe -L -G samples -f 10 -t 2000 -v 160 -u 160 -k 1 -r 1 -m 100 -n 10000 # aupe-global
+
+# with trusted nodes
+cargo run -- -T 10 -n 10 aupe -O -G samples -f 10 -x 3 -t 3 -v 5 -u 5 -k 1 -r 1 -m 10 -n 10
+cargo run -- -T 200 -n 10000 aupe -O -G samples -f 10 -x 100 -t 2000 -v 160 -u 160 -k 1 -r 1 -m 100 -n 10000 
