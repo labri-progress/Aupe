c716c1d3ad105dffab489ccad1c26582f714a1a8
djadar <mukamaugusta5@gmail.com>
2024-08-03 14:38:42 +0200

    test aupe-global on 400 rounds

--- scripts10k.sh
+++ scripts10k.sh
@@ -10,11 +10,11 @@ sm=100
 roundMax=200
 
 mkdir $strat
-if [ $strat -eq "global" ]; then
+if [[ "$strat" == "global" ]];
     # f=22% rho=1
     cargo run -- -T 400 -n 10000 aupe -L -G samples -f $force -t 2200 \
         -v 160 -u 160 -k 1 -r 1 > $strat"/rho0text22"
-elif [ $strat -eq "merge" ]; then
+elif [[ "$strat" == "merge" ]]; then
     # f=22% rho=1
     cargo run -- -T 400 -n 10000 aupe -O -G samples -f $force -t 2200 \
         -v 160 -u 160 -k 1 -r 1 > $strat"/rho0text22"
