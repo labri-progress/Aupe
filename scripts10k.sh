770279cbbe551dc7f87ff6b789b63ee831ce1f15
djadar <mukamaugusta5@gmail.com>
2024-08-04 10:06:44 +0200

    try strategies for 600 rounds

--- scripts10k.sh
+++ scripts10k.sh
@@ -19,15 +19,26 @@ elif [ $strat -eq 4 ]; then
     stratLitt="aupewithT"
 elif [ $strat -eq 5 ]; then
     stratLitt="addition"
+elif [ $strat -eq 6 ]; then
+    stratLitt="aupe"
+elif [ $strat -eq 7 ]; then
+    stratLitt="mergeT"
 fi
 echo "received instruction for " $stratLitt
 
 mkdir $stratLitt
 if [ $strat -eq 0 ]; then
-    # f=22% rho=1
-    echo "Globalw400rounds" > $stratLitt"/log.txt"
-    nohup ./aupeglobal -T 400 -n $N aupe -L -G samples -f $force -t 2200 \
-        -v 160 -u 160 -k 1 -r 1 -m $sm -n $N > $stratLitt"/rho1text22" &
+    f=0.22 #"${2:-0}" # 0.22 0.24 0.26 0.28  
+    k="${2:-0}"
+    rnd=600
+    # ./scripts10k.sh 0 1
+    F=$(echo "scale=0; 100.0 * $f / 1" | bc)
+    echo "F="$F" rho="$k " rnd="$rnd
+    byz=$(echo "scale=0; $N * $f / 1" | bc)
+
+    echo "Globalw600rounds" > $stratLitt"/log.txt"
+    nohup ./aupeglobal -T $rnd -n $N aupe -L -G samples -f $force -t $byz \
+        -v 160 -u 160 -k $k -r 1 -m $sm -n $N > $stratLitt"/rho"$k"text"$F &
 
 elif [ $strat -eq 1 ]; then
     # f=22% rho=1
@@ -72,7 +83,7 @@ elif [ $strat -eq 4 ]; then
     echo $folder"/text"$F"-"$T
     trust=$(echo "scale=0; $N * $t / 1" | bc)
 
-    echo "TotalMergewithPonderation" > $stratLitt"/log.txt"
+    echo "TotalMergewithT" > $stratLitt"/log.txt"
     nohup ./aupewitT -T $rnd -n $N aupe -O -G samples -f $force -t $byz -x $trust \
         -v 160 -u 160 -k $k -r 1 -m $sm -n $N > $stratLitt"/rho"$k"text"$F"-"$T &
 
@@ -92,4 +103,35 @@ elif [ $strat -eq 5 ]; then
     echo "TotalMergewithAddition" > $stratLitt"/log.txt"
     nohup ./add -T $rnd -n $N aupe -O -G samples -f $force -t $byz -x $trust \
         -v 160 -u 160 -k $k -r 1 -m $sm -n $N > $stratLitt"/rho"$k"text"$F"-"$T &
+
+elif [ $strat -eq 6 ]; then
+    f=0.22 
+    k="${2:-0}"
+    # ./scripts10k.sh 6 1
+    F=$(echo "scale=0; 100.0 * $f / 1" | bc)
+    echo "F="$F" rho="$k " rnd="$rnd
+    byz=$(echo "scale=0; $N * $f / 1" | bc)
+
+    echo "Aupe" > $stratLitt"/log.txt"
+    nohup ./aupewoponderation -T $rnd -n $N aupe -G samples -f $force -t $byz \
+        -v 160 -u 160 -k $k -r 1 -m $sm -n $N > $stratLitt"/rho"$k"text"$F &
+
+elif [ $strat -eq 7 ]; then
+    f=0.22  
+    t="${2:-0}"
+    k="${3:-0}"
+    rnd=600
+    F=$(echo "scale=0; 100.0 * $f / 1" | bc)
+    echo "F="$F" rho="$k " rnd="$rnd
+    byz=$(echo "scale=0; $N * $f / 1" | bc)
+    # ./scripts10k.sh 7 0.01 1
+
+    T=$(echo "scale=0; 100.0 * $t / 1" | bc)
+    echo $folder"/text"$F"-"$T
+    trust=$(echo "scale=0; $N * $t / 1" | bc)
+
+    echo "MergewT" > $stratLitt"/log.txt"
+    nohup ./aupewitT -T $rnd -n $N aupe -O -G samples -f $force -t $byz -x $trust \
+        -v 160 -u 160 -k $k -r 1 -m $sm -n $N > $stratLitt"/rho"$k"text"$F"-"$T &
+
 fi
