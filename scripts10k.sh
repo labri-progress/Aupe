c2426fe0159f65a6135909656981e32c7f51b45b
djadar <mukamaugusta5@gmail.com>
2024-08-12 19:25:52 +0200

    add conctact list of nodes

--- scripts10k.sh
+++ scripts10k.sh
@@ -16,9 +16,10 @@ mkdir $stratLitt
 if [ $strat -eq 0 ]; then
     f="${2:-0}" # 0.20 0.22 0.24 0.26 0.28 0.30
     t="${3:-0.01}"
-    rnd="${4:-1000}"
-    k="${5:-1}"
-    # ./scripts10k.sh 0 0.26 0.01 [ 1000 1 ]
+    k="${4:-1}"
+    sup="${5:-20}"
+    rnd="${6:-500}"
+    # ./scripts10k.sh 0 0.26 0.01 [ 1 20 500]
     F=$(echo "scale=0; 100.0 * $f / 1" | bc)
     echo "F="$F" rho="$k " rnd="$rnd
     byz=$(echo "scale=0; $N * $f / 1" | bc)
@@ -30,5 +31,5 @@ if [ $strat -eq 0 ]; then
     
     echo $stratLitt > $stratLitt"/log.txt"
     nohup ./aupeTN -T $rnd -n $N aupe -O -G samples -f $force -t $byz -x $trust \
-        -v 160 -u 160 -k $k -r $r -m $sm -n $N > $stratLitt"/rho"$k"text"$F"-"$T &
+        -v 160 -u 160 -k $k -r $r -m $sm -n $N -p $sup > $stratLitt"/rho"$k"text"$F"-"$T &
 fi
