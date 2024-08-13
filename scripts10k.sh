5712878627c7bc70ddb81ed1d5c0ac728cf0a192
djadar <mukamaugusta5@gmail.com>
2024-08-13 09:29:33 +0200

    expe of 500 rounds

--- scripts10k.sh
+++ scripts10k.sh
@@ -9,6 +9,8 @@ rnd=200
 r=1
 if [ $strat -eq 0 ]; then
     stratLitt="TNcontact"
+elif [ $strat -eq 1 ]; then
+    stratLitt="basalt"
 fi
 echo "received instruction for " $stratLitt
 
@@ -32,4 +34,17 @@ if [ $strat -eq 0 ]; then
     echo $stratLitt > $stratLitt"/log.txt"
     nohup ./aupeTN -T $rnd -n $N aupe -O -G samples -f $force -t $byz -x $trust \
         -v 160 -u 160 -k $k -r $r -m $sm -n $N -p $sup > $stratLitt"/rho"$k"text"$F"-"$T &
+
+elif [ $strat -eq 1 ]; then
+    f="${2:-0}" # 0.20 0.22 0.24 0.26 0.28 0.30
+    k="${3:-1}"
+    rnd="${4:-500}"
+    # ./scripts10k.sh 1 0.26 1 [ 500 ]
+    F=$(echo "scale=0; 100.0 * $f / 1" | bc)
+    echo "F="$F" rho="$k " rnd="$rnd
+    byz=$(echo "scale=0; $N * $f / 1" | bc)
+
+    echo "Basalt Rounds" > $stratLitt"/log.txt"
+    nohup ./basalt-sim -T $rnd -n $N basalt-simple -G -f $force -t $byz 500\
+        -v 160 -i 160 -k $k -r 1 > $stratLitt"/rho"$k"text"$F &
 fi
