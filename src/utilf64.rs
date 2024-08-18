9f12202324005ce8e60d3af44ead57b487dfef13
djadar <mukamaugusta5@gmail.com>
2024-08-18 19:22:54 +0200

    redo some aupe global expe

--- src/utilf64.rs
+++ src/utilf64.rs
@@ -1,6 +1,7 @@
 use std::hash::{Hash, Hasher};
 use fasthash::*;
 use rand::{thread_rng, Rng};
+use std::fmt::Write; // Import the Write trait
 
 use super::net::PeerRef;
 
@@ -67,12 +68,12 @@ pub fn sample_nocopy<T: PartialEq + Clone>(from: &mut [T], n: usize) -> Vec<T> {
     from.iter().position(|&x| x == value)
 } */
 
-pub fn get_min_key_value(from: &[isize]) -> Option<(usize, isize)> {
+pub fn get_min_key_value(from: &[f64]) -> Option<(usize, f64)> {
     let mut min_value = None;
     let mut min_index = None;
 
     for (index, &value) in from.iter().enumerate() {
-        if value > 0 {
+        if value > 0.0 {
             match min_value {
                 Some(v) if value < v => {
                     min_value = Some(value);
@@ -102,34 +103,52 @@ pub fn print_samples(sample_view: &mut Vec<(u64, Option<PeerRef>)>) {
     println!("]");
 }
 
-pub fn print_vector_with_two_digits(v: Vec<isize>) {
+pub fn print_vector_with_two_digits(v: Vec<f64>) {
     print!("[");
     for num in v {
         print!("{:.2} ", num);
     }
     print!("]");
 }
-pub fn get_min_key_valuef64(from: &[f64]) -> Option<(usize, f64)> {
-    let mut min_value = None;
-    let mut min_index = None;
 
-    for (index, &value) in from.iter().enumerate() {
-        if value > 0.0 {
-            match min_value {
-                Some(v) if value < v => {
-                    min_value = Some(value);
-                    min_index = Some(index);
-                },
-                None => {
-                    min_value = Some(value);
-                    min_index = Some(index);
-                },
-                _ => {}
-            }
+pub fn vec_to_string(vec: &[f64]) -> String {
+    let precision = 2;
+    let mut result = String::with_capacity(vec.len() * (precision + 3)); // Allocate some capacity to reduce reallocations
+    for (i, num) in vec.iter().enumerate() {
+        if i > 0 {
+            result.push(','); // Append a comma between elements
         }
+        // Use a buffer to format the number directly
+        let _ = write!(&mut result, "{:.1$}", num, precision);
     }
-    match (min_index, min_value) {
-        (Some(i), Some(v)) => Some((i, v)),
-        _ => None,
-    }
+    result
 }
+
+pub fn string_to_vec(s: &str) -> Result<Vec<f64>, std::num::ParseFloatError> {
+    s.split(',')
+        .map(str::trim) // Trim whitespace
+        .map(str::parse::<f64>) // Parse directly
+        .collect()
+}
+
+pub fn vec_to_string_slow(vec: &Vec<f64>) -> String {
+    let precision = 2;
+    vec.iter()
+    .map(|num| format!("{:.1$}", num, precision))
+        .collect::<Vec<String>>()
+        .join(",")
+}
+
+pub fn string_to_vec_slow(s: &str) -> Result<Vec<f64>, std::num::ParseFloatError> {
+    s.split(',')
+        .map(|num_str| num_str.trim().parse::<f64>())
+        .collect()
+}
+
+
+/* fn string_to_vec(s: &str) -> Result<Vec<f64>, std::num::ParseIntError> {
+    s.split(',')
+        .map(|num_str| num_str.trim().parse::<i32>())
+        .collect()
+}
+ */
