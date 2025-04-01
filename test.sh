03c2a1eb64f25c378d166d4fbeca5f6f35869f91
djadar <mukamaugusta5@gmail.com>
2025-04-01 16:56:02 +0200

    test script

---
+++ test.sh
@@ -0,0 +1,3 @@
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 1000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c1
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 2000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c2
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 3000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c3
