767c235dbf9c725274c07383bdffd2209c5aea79
djadar <mukamaugusta5@gmail.com>
2025-04-02 08:16:30 +0200

    update test script

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 mkdir aupe1000
-cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 1000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c1
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 1000  -v 160 -u 160 -m 10 -n 10000  > c1
 mkdir aupe2000
-cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 2000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c2
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 2000  -v 160 -u 160 -m 10 -n 10000  > c2
 mkdir aupe3000
-cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 3000 -x 0 -v 160 -u 160 -m 10 -n 10000 -p 0 > c3
+cargo run -- -T 200 -n 10000 aupe -G samples -f 10 -t 3000  -v 160 -u 160 -m 10 -n 10000  > c3
