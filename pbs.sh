e3bda9b0de24502be961528d4eeb9f22acd00c29
djadar <mukamaugusta5@gmail.com>
2025-04-17 14:12:14 +0200

    fix PBS sketch

--- pbs.sh
+++ pbs.sh
@@ -6,6 +6,7 @@ else
 fi
 
 N=10000
-cargo run -- -T 200 -n $N aupe -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n $N -x 0 -p 0 > p1$extension
-cargo run -- -T 200 -n $N aupe -G samples -f 10 -t 2000 -v 160 -u 160 -m 100 -n $N -x 0 -p 0 > p2$extension
+
 cargo run -- -T 200 -n $N aupe -G samples -f 10 -t 3000 -v 160 -u 160 -m 100 -n $N -x 0 -p 0 > p3$extension
+cargo run -- -T 200 -n $N aupe -G samples -f 10 -t 2000 -v 160 -u 160 -m 100 -n $N -x 0 -p 0 > p2$extension
+cargo run -- -T 200 -n $N aupe -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n $N -x 0 -p 0 > p1$extension
