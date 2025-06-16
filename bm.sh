68c9be021c203cc95ca8e36ca93de08913981fd7
djadar <mukamaugusta5@gmail.com>
2025-06-16 09:06:07 +0200

    add xpe for kvs cf and bm

--- bm.sh
+++ bm.sh
@@ -7,12 +7,12 @@ fi
 
 space="${2:-6}"
 
-N=10000
+N=50000
 
 # cargo run -- -T 200 -n 1000 bm -G samples -f 10 -t 100 -v 20 -u 20 -m 100 -n 1000 -y 6
 for strat in "kvs" "bm" "cf"
 do
-    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 3000 -v 160 -u 160 -m 100 -n $N -d 12 > $strat"3"$extension
-    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 2000 -v 160 -u 160 -m 100 -n $N -d 12 > $strat"2"$extension
-    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n $N -d 12 > $strat"1"$extension
+    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 15000 -v 160 -u 160 -m 100 -n $N -y $space > $strat"3"$extension
+    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 10000 -v 160 -u 160 -m 100 -n $N -y $space > $strat"2"$extension
+    cargo run -- -T 200 -n $N $strat -G samples -f 10 -t 5000 -v 160 -u 160 -m 100 -n $N -y $space > $strat"1"$extension
 done
