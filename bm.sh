f1313b28ecb4d3ee990bb6c791ebb8fad344648f
djadar <mukamaugusta5@gmail.com>
2025-08-26 02:50:43 +0200

    add basalt

--- bm.sh
+++ bm.sh
@@ -8,8 +8,10 @@ N=10000
 round=2000
 mkdir analysis
 # cargo run -- -T 2000 -n 10000 bm -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n 10000 -y 10 > "analysis/bm-10-1000-10"
-#cargo run -- -T 2000 -n 10000 bm -G samples -f 2 -t 2000 -v 160 -u 160 -m 100 -n 10000 -y 40 > "analysis/bm-2-2000-40"
 # cargo run -- -T 2000 -n 10000 kvs -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n 10000  > "analysis/kvs-10-1000
+# cargo run -- -T 2000 -n 10000 brahms -G samples -f 10 -t 1000 -v 160 -u 160 
+# cargo run -- -T 2000 -n 10000 basalt -G -f 10 -t 3000 -v 160 -i 160 
+
 for strat in "bm" #"kvs" "bm" "cf"
 do
     for space in $space #10 20 30 40
