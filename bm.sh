c9c8296b40aaf796cd068f14539641cc2a027533
djadar <mukamaugusta5@gmail.com>
2025-09-01 01:17:57 +0200

    new points for faulty

--- bm.sh
+++ bm.sh
@@ -20,16 +20,16 @@ do
     for faulty in 1200 1400 2600 2800 # 800 1200 1400 1600 1800 2200 2400 2600 2800 4000 # 1000 2000 3000 # 4000 5000
     do
         if [ "$strat" == "kvs" ]; then
-            cargo run -- -T $round -n $N kvs -G samples -f $ratio -t $faulty -v 160 -u 160 -m 100 -n $N > "analysis/kvs-$ratio-$faulty-$space"
+            cargo run -- -T $round -n $N kvs -G samples -f $ratio -t $faulty -v 160 -u 160 -m 100 -n $N > "analysis/kvs-$ratio-$faulty"
         fi
         if [ "$strat" == "bm" ]; then
             cargo run -- -T $round -n $N bm -G samples -f $ratio -t $faulty -v 160 -u 160 -m 100 -n $N -y $space > "analysis/bm-$ratio-$faulty-$space"
         fi
         if [ "$strat" == "basalt" ]; then
-            cargo run -- -T $round -n $N basalt -G samples -f $ratio -t $faulty -v 160 -i 160 -k 1 -r 1 > "analysis/bs-$ratio-$faulty-$space"
+            cargo run -- -T $round -n $N basalt -G -f $ratio -t $faulty -v 160 -i 160 -k 1 -r 1 > "analysis/bs-$ratio-$faulty"
         fi
         if [ "$strat" == "brahms" ]; then
-            cargo run -- -T $round -n $N brahms -G samples -f $ratio -t $faulty -v 160 -u 160 > "analysis/br-$ratio-$faulty-$space"
+            cargo run -- -T $round -n $N brahms -G samples -f $ratio -t $faulty -v 160 -u 160 > "analysis/br-$ratio-$faulty"
         fi
     done
 done
