1236e8f735654ad8077413befd6e7f63629f8b44
djadar <mukamaugusta5@gmail.com>
2025-09-01 14:04:38 +0200

    200 rounds

--- bm.sh
+++ bm.sh
@@ -8,7 +8,7 @@ space="${2:-10}"
 ratio=10
 
 N=10000
-round=2000
+round=200
 mkdir analysis
 # cargo run -- -T 2000 -n 10000 bm -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n 10000 -y 10 > "analysis/bm-10-1000-10"
 # cargo run -- -T 2000 -n 10000 kvs -G samples -f 10 -t 1000 -v 160 -u 160 -m 100 -n 10000  > "analysis/kvs-10-1000
@@ -17,7 +17,7 @@ mkdir analysis
 
 for space in $space #1 5 10 20 30 40
 do
-    for faulty in 1200 1400 2600 2800 # 800 1200 1400 1600 1800 2200 2400 2600 2800 4000 # 1000 2000 3000 # 4000 5000
+    for faulty in 800 1200 1400 1600 1800 2200 2400 2600 2800 # 800 1200 1400 1600 1800 2200 2400 2600 2800 4000 # 1000 2000 3000 # 4000 5000
     do
         if [ "$strat" == "kvs" ]; then
             cargo run -- -T $round -n $N kvs -G samples -f $ratio -t $faulty -v 160 -u 160 -m 100 -n $N > "analysis/kvs-$ratio-$faulty"
