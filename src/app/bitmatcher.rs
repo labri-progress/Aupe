f9200767ef93af812bac41ac509685459921db69
djadar <mukamaugusta5@gmail.com>
2025-09-28 17:17:32 +0200

    aupe bitmatcher merge done

--- src/app/bitmatcher.rs
+++ src/app/bitmatcher.rs
@@ -93,7 +93,7 @@ impl BM {
     }
     
     pub fn print(& self) {
-        println!("BM of 2 arrays, each of {:?} buckets of size 64 bits", self.params.n_bucket); 
+        //println!("BM of 2 arrays, each of {:?} buckets of size 64 bits", self.params.n_bucket); 
         self.matrix.print_buckets();
         //println!("\nmin_value {}", self.min_value);
     }
