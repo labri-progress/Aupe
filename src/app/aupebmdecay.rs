31fed6836805689f7a43e7b8c346162c5d239eeb
djadar <mukamaugusta5@gmail.com>
2026-01-28 22:07:59 +0100

    remove print in code

--- src/app/aupebmdecay.rs
+++ src/app/aupebmdecay.rs
@@ -476,10 +476,10 @@ impl App for AupeDecay {
                         }
                     }
 
-                    if self.my_id == self.params.n_byzantine && net.time()%200==0 { //} && (net.time()==1 || net.time()==200) {//+ self.params.n_trusted -1{
+                    /* if self.my_id == self.params.n_byzantine && net.time()%200==0 { //} && (net.time()==1 || net.time()==200) {//+ self.params.n_trusted -1{
 
                         self.sketch.print();
-                    }
+                    } */
                     net.send(self.my_id, Msg::SelfNotif);
                 },
                 Msg::PullRequest => {
