4a113745c7ab7f5167ea43d4d68280dc850b4405
djadar <mukamaugusta5@gmail.com>
2026-01-28 22:05:39 +0100

    add expe shell and r script

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ pub enum WhichApp {
     AupeDecay(app::aupebmdecay::Init),
 
     /// Aupe RPS
-    #[structopt(name = "kvs")]
+    #[structopt(name = "array")]
     Aupe(app::aupe::Init),
 
     /// Brahms RPS
